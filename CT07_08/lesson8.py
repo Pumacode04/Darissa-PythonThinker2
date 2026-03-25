0f8b420637261462beea4377a75377750def00bc
Pumacode04 <cransonlim@yahoo.com.sg>
2026-03-25 09:02:29 +0000

    11th of March, 2026

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -101,16 +101,117 @@
 #        print("Invalid. Try Again.")
 
 #Task 4a:
-sentence = ""
-mocking = ""
-counter = 0
-sentence = input("Give me a sentence.")
-for char in sentence:
-    if char.isalpha:
-        if counter / 2 == 0:
-            char = char.upper()
-        else:
-            char = char.lower()
-        mocking = char + mocking
-        counter += 1
-print(mocking)
+#sentence = ""
+#mocking = ""
+#counter = 0
+#sentence = input("Give me a sentence.")
+#for char in sentence:
+#    if char.isalpha():
+#        if counter % 2 == 0:
+#            char = char.upper()
+#        else:
+#            char = char.lower()
+#        mocking = mocking + char
+#        counter += 1
+#    else:
+#        mocking = mocking + char
+#print(mocking)
+
+#Task 5:
+#word = "SINGAPORE"
+#print(word[0:4])
+#print(word[3:6])
+#print(word[5:])
+#print(word[0:9:2])
+
+#Task 6:
+#word = ""
+#reverso = word[::-1]
+#while True:
+#    word = input("Enter a word.")
+#    reverso = word[::-1]
+#    if word == "end":
+#        break
+#    if word == reverso:
+#        print("This word is a panlindrone.")
+#    else:
+#        print("This word is not a panlindrone.")
+
+#Task 7:
+#friends = ["Amanda", "Braelyn", "Cheryl"]
+#user = input("Name : ")
+#while user.strip() == "":
+#    print("Invaild name. Try Again.")
+#    user = input("Name : ")
+#
+#if user in friends:
+#    print("You may come in.")
+#else:
+#    print("Go Home.")
+
+#Task 8:
+#first_last_upper = False
+#first_letter_valid = False
+#digit_7 = False
+#char_9 = False
+
+#while True:
+#    nric = input("Enter your nric.")
+#    if nric.strip() == "":
+#        print("NRIC cannot be empty. Try Again.")
+#    elif len(nric) != 9:
+#        print("NRIC should have 9 characters. Try Again.")
+#    else:
+#        char_9 = True
+#        print("Has 9 char")
+#        break
+
+#if nric[0].isupper() and nric[8].isupper():
+#    first_last_upper = True
+#    print("First and last upper")
+#if nric[0] == "S" or nric[0] == "T" or nric[0] == "F" or nric[0] == "G" or nric[0] == "M":
+#    first_letter_vaild = True
+#    print("First letter valid")
+#if nric[1:8].isdigit():
+#    digit_7 = True
+#    print("Has 7 num")
+##if len(nric) == 9:
+##    char_9 = True
+
+#if first_last_upper and first_letter_vaild and digit_7 and char_9:
+#    print("NRIC is vaild.")
+#else:
+#    print("NRIC is NOT vaild.")
+
+#Task 9:
+char8 = False
+upLow = False
+num = False
+upper = False
+lower = False
+noSpecialChar = False
+
+password = input("Enter a password. ")
+if len(password) >= 8:
+    char8 = True
+
+for char in password:
+    if char.isalpha():
+        if char.isupper():
+            upper = True
+        if char.islower():
+            lower = True
+    if upper and lower:
+        upLow = True
+    if char.isdigit():
+        num = True
+    if char.isalpha() or char.isdigit():
+        noSpecialChar = True
+    else:
+        noSpecialChar = False
+        break
+
+if char8 and upLow and num and noSpecialChar:
+    print("Password successfully set.")
+else:
+    print("Your password did not meet requirements.")
--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,2 +1,37 @@
-print("Hello from lesson 9")
+# print("Hello from lesson 9")
 
+#Recap 1:
+#correct = False
+#answer = input("What has to be broken before you can use it? ")
+#answer = answer.lower()
+#if "egg" in answer:
+#    correct = True
+#if correct:
+#    print("Good.")
+#else:
+#    print("Try Again.")
+
+#Task 1:
+#Import Libraries
+import turtle
+#Global Variables
+screen = turtle.Screen()
+t = turtle.Turtle()
+t.speed(0)
+t.penup()
+#Functions
+def draw_square():
+    t.seth(0)
+    t.pendown()
+    for i in range(4):
+        t.forward(100)
+        t.right(90)
+#Main Code:
+t.speed(0)
+t.penup()
+
+draw_square()
+t.forward(100)
+draw_square()
+
+turtle.done()
